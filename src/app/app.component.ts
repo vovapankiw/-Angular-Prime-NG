0fc827be1c7fd10e69025dbb998485609a2a696b
Volodymyr Pankiv <vpanki@softserveinc.com>
2024-08-23 18:33:05 +0300

    added basic configuration
    Signed-off-by: Volodymyr Pankiv <vpanki@softserveinc.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { ButtonGroupModule } from 'primeng/buttongroup';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, ButtonGroupModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'primeng';
